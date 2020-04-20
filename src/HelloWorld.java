27a8df4fcb069b000f941adce47de60913b362d5
TaoheedKing <tking92@montgomerycollege.edu>
2020-04-20 16:55:15 -0400

    Added the current date

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Today is July 19, 2015. I am alive!");
+        System.out.println("Today is April 20, 2020. I am alive!");
     }
 }
